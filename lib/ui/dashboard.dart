9bf5fbb207e1bbdf6030da5b338548cbb66e3f21
ajasadipura3 <ajasadipura3@gatech.edu>
2023-10-15 03:17:16 -0400

    pie chart and swag screen

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+import 'swag.dart'; // Importing swag.dart
 
 class Dashboard extends StatelessWidget {
   @override
@@ -8,13 +11,46 @@ class Dashboard extends StatelessWidget {
         title: Text('Dashboard'),
       ),
       body: Center(
-        child: Text(
-          'DASHBOARD',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+        child: RegularPieChartSample(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the Swag screen when the button is pressed
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Swag()),
+          );
+        },
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
+
+class RegularPieChartSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(
+      PieChartData(
+        sections: [
+          PieChartSectionData(
+            color: Colors.redAccent,
+            value: 30, // Replace with your first variable value
+            title: 'Spendings',
+            radius: 200,
+            titleStyle: TextStyle(color: Colors.white),
           ),
-        ),
+          PieChartSectionData(
+            color: Colors.purple,
+            value: 70, // Replace with your second variable value
+            title: 'Earnings',
+            radius: 200,
+            titleStyle: TextStyle(color: Colors.white),
+          ),
+        ],
+        centerSpaceRadius: 0, // Set to zero for a regular pie chart
+        borderData: FlBorderData(show: false),
+        sectionsSpace: 0,
       ),
     );
   }
